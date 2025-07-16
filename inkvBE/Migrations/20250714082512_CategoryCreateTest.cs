a3be3cab5b784a069c6cb22897043c4b194687a6
armkro <Armandas7080@gmail.com>
2025-07-16 12:54:42 +0300

    Created Category, Subcategory, Type entities and seeded them with data

---
+++ inkvBE/Migrations/20250714082512_CategoryCreateTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace inkvBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryCreateTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subcategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SubcategoryName = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subcategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TypeName = table.Column<string>(type: "text", nullable: false),
+                    SubcategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Types_Subcategories_SubcategoryId",
+                        column: x => x.SubcategoryId,
+                        principalTable: "Subcategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategories_CategoryId",
+                table: "Subcategories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Types_SubcategoryId",
+                table: "Types",
+                column: "SubcategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Types");
+
+            migrationBuilder.DropTable(
+                name: "Subcategories");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ inkvBE/Migrations/20250716080547_TypeSeeding.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inkvBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypeSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Types",
+                columns: new string[] { "Id", "TypeName", "SubcategoryId" },
+                values: new object[,]
+                {
+                    { 1, "Automobilių supirkimas", 1 },
+                    { 2, "Acura", 1 },
+                    { 3, "Aixam", 1 },
+                    { 4, "Alfa Romeo", 1 },
+                    { 5, "Alpina", 1 },
+                    { 6, "Aston Martin", 1 },
+                    { 7, "Audi", 1 },
+                    { 8, "Baic", 1 },
+                    { 9, "BELLIER", 1 },
+                    { 10, "Bentley", 1 },
+                    { 11, "BMW", 1 },
+                    { 12, "Buick", 1 },
+                    { 13, "Cadillac", 1 },
+                    { 14, "Chevrolet", 1 },
+                    { 15, "Chrysler", 1 },
+                    { 16, "Citroen", 1 },
+                    { 17, "Cupra", 1 },
+                    { 18, "Dacia", 1 },
+                    { 19, "Daewoo", 1 },
+                    { 20, "Daihatsu", 1 },
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
