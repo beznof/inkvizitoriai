7a6c5ae53e69b308cca858c0d82ee97d362ee7bb
armkro <Armandas7080@gmail.com>
2025-07-26 16:19:24 +0300

    Created entity ListingStatus

---
+++ inkvBE/Migrations/20250726131531_ListingStatusSeeding.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inkvBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class ListingStatusSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ListingStatuses",
+                column: "Status",
+                values: new string[] {
+                    "available",
+                    "reserved",
+                    "sold"
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
